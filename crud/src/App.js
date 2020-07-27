31f61025f9d576e2367b388c77b9551da4b348b3
Brie <briecardo@icloud.com>
2020-07-27 11:30:28 -0700

    Hello World reflects on react

---
+++ crud/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export const App = () => (
+    <div className="App">
+      <header className="App-header">
+      <p>Hello World</p>
+      </header>
+    </div>
+  );
+
+
+
