43b0ce32647d0eda4a45c652d9274b36b7f14415
Brie <briecardo@icloud.com>
2020-07-27 12:19:57 -0700

    Sidebar reflects on react

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
 import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
 
 export const App = () => (
-    <div className="App">
-    <Header/>
-    </div>
-  );
-
-
-
+  <div className="App">
+          <Header />
+          <Content />
+  </div>
+);
